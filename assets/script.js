282fafcc50cc22733999843d2fce9b6435bb0218
jennyk1989 <jennifer0keene@gmail.com>
2021-12-01 20:35:04 -0600

    pseudocode

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+/* open planner --> current day is display at top of cal*/
+
+// each time block is color-coded to indicate whether it is in the past, present, or future
+
+// click into a time block & enter an event
+
+// click the save button, text for that event is saved in local storage
+
+// refresh the page & the saved events persist
+
