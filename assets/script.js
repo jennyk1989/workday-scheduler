debd6f6299a96a6e00f32f21680cf616145eeb02
jennyk1989 <jennifer0keene@gmail.com>
2021-12-04 11:01:56 -0600

    added pseudo code to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
-//when open planner...(doc needs to load when page is opened)
-
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-
-/* open planner --> current day is display at top of cal*/
+// open planner --> current day is display at top of cal
 //id = currentDay
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 
 // each time block is color-coded to indicate whether it is in the past, present, or future
-//gray = past, red = now, green = coming up 
+//gray = past (.past), red = now (.present), green = coming up (.future)
+
+
+
+
+
+
+
 
 // click into a time block & enter an event
 
