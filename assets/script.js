dbac0aacd4fe7f51b348907a48fb1f5f8f95449d
jennyk1989 <jennifer0keene@gmail.com>
2021-12-03 22:27:13 -0600

    moved js src to end

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,10 @@
+//when open planner...(doc needs to load when page is opened)
+$(function() {});
+
 /* open planner --> current day is display at top of cal*/
+//id = currentDay
+$("#currentDAY").text(moment().format("dddd, MMMM Do YYYY"));
+
 
 // each time block is color-coded to indicate whether it is in the past, present, or future
 //gray = past, red = now, green = coming up 
