3cda33897f45726b1224ff68c4d07d0a5aa1cd9c
jennyk1989 <jennifer0keene@gmail.com>
2021-12-05 12:19:56 -0600

    made 1pm to 5pm ids into integers consecutive to previous ids to fix time block colors

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,48 @@
-//doc ready function
+//only run the javascript code when the page is ready for it to execute (once it's loaded)
+$(document).ready(function() {
+    // open planner --> current day is display at top of cal
+    //id = currentDay
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+    
+    //CSS predefined = description,time-block,row,hour,past,present,future,saveBtn
+    
+    // each time block is color-coded to indicate whether it is in the past, present, or future
+    // gray = past (.past), red = now (.present), green = coming up (.future)
+    function timeUpdate() {
+        //moment method to get the hours from the current time
+        let presentHour = moment().hour();
+    
+        //each method to loop through the time blocks
+        $(".time-block").each(function() {
+            //timeblock's id's...hour9, hour10, hour11, etc
+            //get the integer out of the id of the timeblocks (this)
+            let timeblockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(timeblockHour, presentHour);
+    
+            //if the timeblock's hour has past, make the timeblock grey (.past)
+            if (timeblockHour < presentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            //if the timeblock's hour the current hour, make timeblock red (.present)
+            } else if (timeblockHour === presentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            //if timeblock time is in the future, make timeblock green (.future)
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        });
+    }
+    timeUpdate();
+    // click into a time block & enter an event
+    
+    // click the save button, text for that event is saved in local storage
+    
+    // refresh the page & the saved events persist
 
-// open planner --> current day is display at top of cal
-//id = currentDay
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
-
-//CSS predefined = description,time-block,row,hour,past,present,future,saveBtn
-
-// each time block is color-coded to indicate whether it is in the past, present, or future
-// gray = past (.past), red = now (.present), green = coming up (.future)
-function timeUpdate() {
-    //moment method to get the hours from the current time
-    let presentHour = moment().hour();
-
-    //each method to loop through the time blocks
-    $(".time-block").each(function() {
-        //timeblock's id's...hour9, hour10, hour11, etc
-        //get the integer out of the id of the timeblocks (this)
-        let timeblockHour = parseInt($(this).attr("id").split("hour")[1]);
-
-        //if the timeblock's hour has past, make the timeblock grey (.past)
-        if (timeblockHour < presentHour) {
-            $(this).addClass("past");
-        //if the timeblock's hour the current hour, make timeblock red (.present)
-        } else if (timeblockHour === presentHour) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-        //if timeblock time is in the future, make timeblock green (.future)
-        } else {
-            $(this).removeClass("past");
-            $(this).removeClass("present");
-            $(this).addClass("future");
-        }
-    });
-}
-
-
-
-
-
-
-
-// click into a time block & enter an event
-
-// click the save button, text for that event is saved in local storage
-
-// refresh the page & the saved events persist
+});
 
